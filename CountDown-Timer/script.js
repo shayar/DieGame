00a325e22a9ffe28727e9071b8c102a8b223f83c
shayar-monotype <shayar.shrestha@monotype.com>
2020-09-06 15:13:50 +0545

    Changes

--- CountDown-Timer/script.js
+++ CountDown-Timer/script.js
@@ -2,11 +2,14 @@ const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
+const calculateDiv = document.getElementById('calculatediv');
+const countdownDiv = document.getElementById('countdown');
+const calculateBtn = document.getElementById('Calculate');
 
-const newYears = '1 Jan 2021';
+let randomDay = randomDate();
 
 function countDown() {
-    let newYearsDate = new Date(newYears);
+    let newYearsDate = new Date(randomDay);
     const currentDate = new Date();
     
         const totalSeconds = (newYearsDate - currentDate) / 1000;
@@ -21,9 +24,38 @@ function countDown() {
         secondsEl.innerHTML = formatTime(seconds);
 }
 
+function randomDate() {
+    let startDate = new Date();
+    let endDate = new Date('1 Jan 2040');
+
+    return new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
+}
+
 function formatTime(time) {
     return time < 10 ? (`0${time}`) : time;
 }
 
-countDown();
-setInterval(countDown, 1000);
+function selectedDate() {
+    let x = document.getElementById('date-picker').value;
+    return x;
+}
+
+
+calculateBtn.addEventListener('click', () => {
+    let selectedDates = selectedDate();
+    if(!selectedDates)
+    {
+        alert('Selected date');
+    }
+    else {
+        calculateDiv.style.display = "none";
+        countdownDiv.style.display = "block";
+        countDown();
+        setInterval(countDown, 1000);
+    }
+});
+
+
+
+
+
